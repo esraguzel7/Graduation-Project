436180edcfc4491165d8b933f0e0f0808853af22
mhmttemin <mhmttemin@hotmail.com>
2025-03-18 11:44:48 +0300

    create main mail template

---
+++ resources/views/mails/main.blade.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <!--[if !mso]><!-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!--<![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="x-apple-disable-message-reformatting" />
+    <title>{{ $subject ?? config('app.name') }}</title>
+    <style type="text/css">
+      @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap');
+      html{width:100%}
+      p{margin:0!important}
+      body{-webkit-text-size-adjust:none;-ms-text-size-adjust:none;margin:0;padding:0;font-family:sans-serif!important;background-color:#F6F8FA}
+      table{border-spacing:0;table-layout:auto;margin:0 auto}
+      img{display:block!important;overflow:hidden!important}
+      a{text-decoration:none;color:unset}
+      .ReadMsgBody{width:100%;background-color:#F6F8FA}
+      .ExternalClass{width:100%;background-color:#F6F8FA}
+      .ExternalClass,.ExternalClass p,.ExternalClass span,.ExternalClass font,.ExternalClass td,.ExternalClass div{line-height:100%}
+      .yshortcuts a{border-bottom:none!important}
+      .pad{width:92%}
+      @media only screen and (max-width: 673px) {
+      .res-pad{width:92%;max-width:92%}
+      .res-full{width:100%;max-width:100%}
+      .res-text-left{text-align:left!important}
+      .res-text-right{text-align:right!important}
+      .res-text-center{text-align:center!important}
+      .res-float-left{float:left!important}
+      .res-float-right{float:right!important}
+      .res-float-unset{float:unset!important}
+      .mc-fluid-img{max-width:100%!important}
+      }@media only screen and (max-width: 770px) {
+      .res-rem-radius{border-radius:0!important}
+      .margin-full{width:100%;max-width:100%}
+      .margin-pad{width:92%;max-width:92%;max-width:600px}
+      }
+    </style>
+  </head>
+  <body>
+    <table border="0" cellpadding="0" cellspacing="0" align="center" bgcolor="#F6F8FA" width="100%">
+      <tbody>
+        <tr>
+          <td>
+            <table border="0" cellpadding="0" cellspacing="0" align="center" class="margin-full" width="750" style="margin-top:-1px;">
+              <tbody>
+                <tr>
+                  <td>
+                    <table border="0" cellpadding="0" cellspacing="0" align="center" class="margin-pad" width="600">
+                      <tbody>
+                        <tr><td height="30" style="font-size:0;line-height:0;">&nbsp;</td></tr>
+                        <tr>
+                          <td style="padding-top:0;padding-bottom:0;">
+                            <table border="0" cellpadding="0" cellspacing="0" align="center" class="res-full">
+                              <tbody>
+                                <tr>
+                                  <td>
+                                    <table border="0" cellpadding="0" cellspacing="0" align="center">
+                                      <tbody>
+                                        <tr>
+                                          <td class="res-text-center" style="text-align:center;padding-right:4px;padding-left:0;"><a style="font-family:'Roboto', Arial;font-size:16px;line-height:22px;letter-spacing:0px;color:#252B3A;word-break:break-word;text-decoration:none;" href="">Can't you see this email?</a></td>
+                                          <td class="res-text-center" style="text-align:center;padding-right:0;padding-left:4px;">
+                                            <a style="font-family:'Roboto', Arial;font-size:16px;line-height:22px;letter-spacing:0px;color:#252B3A;word-break:break-word;text-decoration:none;" href="#"><span style="color:#3162D4;font-weight:bold;">Webview</span></a>
+                                          </td>
+                                        </tr>
+                                      </tbody>
+                                    </table>
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </td>
+                        </tr>
+                        <tr><td height="30" style="font-size:0;line-height:0;">&nbsp;</td></tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    @yield('content')
+
+    <table border="0" cellpadding="0" cellspacing="0" align="center" bgcolor="#F6F8FA" width="100%">
+      <tbody>
+        <tr>
+          <td>
+            <table border="0" cellpadding="0" cellspacing="0" align="center" bgcolor="#F0F3F6" class="margin-full res-rem-radius" width="750" style="border-radius:0px 0px 8px 8px;margin-top:-1px;">
+              <tbody>
+                <tr>
+                  <td>
+                    <table border="0" cellpadding="0" cellspacing="0" align="center" class="margin-pad" width="600">
+                      <tbody>
+                        <tr><td height="65" style="font-size:0;line-height:0;">&nbsp;</td></tr>
+                        <tr>
+                          <td width="64" valign="top" style="vertical-align:top;padding-top:0;padding-bottom:18px;">
+                            <table border="0" cellpadding="0" cellspacing="0" align="center" class="res-full">
+                              <tbody>
+                                <tr>
+                                  <td>
+                                    <table border="0" cellpadding="0" cellspacing="0" align="center">
+                                      <tbody>
+                                        <tr>
+                                          <td><img alt="Image" width="64" src="img/9fe4801ae10ed0e845816d8a43c6404d.png" style="font-size:0;line-height:0;width:100%;display:block;max-width:64px;border-radius:4px;border:0;" /></td>
+                                        </tr>
+                                      </tbody>
+                                    </table>
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style="padding-top:7px;padding-bottom:7px;">
+                            <table border="0" cellpadding="0" cellspacing="0" align="center" class="res-full">
+                              <tbody>
+                                <tr>
+                                  <td>
+                                    <table border="0" cellpadding="0" cellspacing="0" align="center">
+                                      <tbody>
+                                        <tr>
+                                          <td>
+                                            <table border="0" cellpadding="0" cellspacing="0" style="padding:6px 6px 6px 6px;border:3px solid #252B3A;border-radius:31px;">
+                                              <tbody>
+                                                <tr>
+                                                  <td align="center" style=""><img alt="Icon" width="25" src="img/824c45b21e95530caf2eaef3d3f8c432.png" style="border:0;font-size:0;line-height:0;max-width:25px;display:block;" /></td>
+                                                </tr>
+                                              </tbody>
+                                            </table>
+                                          </td>
+                                          <td style="width:0;font-size:0;line-height:0;padding-left:7px;">&nbsp;</td>
+                                          <td>
+                                            <table border="0" cellpadding="0" cellspacing="0" style="padding:6px 6px 6px 6px;border:3px solid #252B3A;border-radius:31px;">
+                                              <tbody>
+                                                <tr>
+                                                  <td align="center" style=""><img alt="Icon" width="25" src="img/051447cf531250b28969ab64dbaa2ce6.png" style="border:0;font-size:0;line-height:0;max-width:25px;display:block;" /></td>
+                                                </tr>
+                                              </tbody>
+                                            </table>
+                                          </td>
+                                          <td style="width:0;font-size:0;line-height:0;padding-left:7px;">&nbsp;</td>
+                                          <td>
+                                            <table border="0" cellpadding="0" cellspacing="0" style="padding:6px 6px 6px 6px;border:3px solid #252B3A;border-radius:31px;">
+                                              <tbody>
+                                                <tr>
+                                                  <td align="center" style=""><img alt="Icon" width="25" src="img/3fe95099105fc314f5e471df00bdca11.png" style="border:0;font-size:0;line-height:0;max-width:25px;display:block;" /></td>
+                                                </tr>
+                                              </tbody>
+                                            </table>
+                                          </td>
+                                          <td style="width:0;font-size:0;line-height:0;padding-left:7px;">&nbsp;</td>
+                                          <td>
+                                            <table border="0" cellpadding="0" cellspacing="0" style="padding:6px 6px 6px 6px;border:3px solid #252B3A;border-radius:31px;">
+                                              <tbody>
+                                                <tr>
+                                                  <td align="center" style=""><img alt="Icon" width="25" src="img/787085c50ecab2651e2531767b5d2230.png" style="border:0;font-size:0;line-height:0;max-width:25px;display:block;" /></td>
+                                                </tr>
+                                              </tbody>
+                                            </table>
+                                          </td>
+                                        </tr>
+                                      </tbody>
+                                    </table>
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style="padding-top:32px;padding-bottom:16px;">
+                            <table border="0" cellpadding="0" cellspacing="0" align="center" width="100%">
+                              <tbody>
+                                <tr><td height="0" style="border-bottom:2px solid rgba(37, 43, 58, 0.25);border-radius:2px;"></td></tr>
+                              </tbody>
+                            </table>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style="padding-top:0;padding-bottom:0;">
+                            <table border="0" cellpadding="0" cellspacing="0" align="center" class="res-full">
+                              <tbody>
+                                <tr>
+                                  <td>
+                                    <table border="0" cellpadding="0" cellspacing="0" align="center">
+                                      <tbody>
+                                        <tr>
+                                          <td class="res-text-center" style="text-align:center;"><a style="font-family:'Roboto', Arial;font-size:16px;line-height:22px;letter-spacing:0px;color:#252B3A;word-break:break-word;text-decoration:none;" href="#">Mailseditor All Rights Reserved</a></td>
+                                          <td align="center" style="padding-right:11px;padding-left:11px;"><img alt="Icon" width="6" src="img/2555f135b47aed8c6a25d743268f5e75.png" style="border:0;font-size:0;line-height:0;max-width:6px;display:block;" /></td>
+                                          <td class="res-text-center" style="text-align:center;"><a style="font-family:'Roboto', Arial;font-size:16px;line-height:22px;letter-spacing:0px;color:#252B3A;word-break:break-word;text-decoration:none;" href="#">Terms & Conditions</a></td>
+                                        </tr>
+                                      </tbody>
+                                    </table>
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style="padding-top:16px;padding-bottom:22px;">
+                            <table border="0" cellpadding="0" cellspacing="0" align="center" width="100%">
+                              <tbody>
+                                <tr><td height="0" style="border-bottom:2px solid rgba(37, 43, 58, 0.25);border-radius:2px;"></td></tr>
+                              </tbody>
+                            </table>
+                          </td>
+                        </tr>
+                        <tr><td height="65" style="font-size:0;line-height:0;">&nbsp;</td></tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <table border="0" cellpadding="0" cellspacing="0" align="center" bgcolor="#F6F8FA" width="100%">
+      <tbody>
+        <tr>
+          <td>
+            <table border="0" cellpadding="0" cellspacing="0" align="center" class="margin-full" width="750" style="margin-top:-1px;">
+              <tbody>
+                <tr>
+                  <td>
+                    <table border="0" cellpadding="0" cellspacing="0" align="center" class="margin-pad" width="600">
+                      <tbody>
+                        <tr><td height="30" style="font-size:0;line-height:0;">&nbsp;</td></tr>
+                        <tr>
+                          <td style="padding-top:0;padding-bottom:0;">
+                            <table border="0" cellpadding="0" cellspacing="0" align="center" class="res-full">
+                              <tbody>
+                                <tr>
+                                  <td>
+                                    <table border="0" cellpadding="0" cellspacing="0" align="center">
+                                      <tbody>
+                                        <tr>
+                                          <td class="res-text-center" style="text-align:center;padding-right:4px;padding-left:0;"><a style="font-family:'Roboto', Arial;font-size:16px;line-height:22px;letter-spacing:0px;color:#252B3A;word-break:break-word;text-decoration:none;" href="#">Bu mail yanlış mı geldi?</a></td>
+                                          <td class="res-text-center" style="text-align:center;padding-right:0;padding-left:4px;">
+                                            <a style="font-family:'Roboto', Arial;font-size:16px;line-height:22px;letter-spacing:0px;color:#252B3A;word-break:break-word;text-decoration:none;" href="#"><span style="color:#3162D4;font-weight:bold;">Abonelikten Çık</span></a>
+                                          </td>
+                                        </tr>
+                                      </tbody>
+                                    </table>
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </td>
+                        </tr>
+                        <tr><td height="30" style="font-size:0;line-height:0;">&nbsp;</td></tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </body>
+</html>
