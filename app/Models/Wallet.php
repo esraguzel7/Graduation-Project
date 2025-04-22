0cae22f6fe10657fafca81a126c5f5977ac6622e
Esra Güzel <esraravensburg@gmail.com>
2025-04-22 15:20:09 +0300

    Create Wallet model and table

---
+++ app/Models/Wallet.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Wallet extends Model
+{
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function cards()
+    {
+        return $this->hasMany(Card::class);
+    }
+
+    public function cardRequests()
+    {
+        return $this->hasMany(CardRequest::class);
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(WalletTransaction::class);
+    }
+}
