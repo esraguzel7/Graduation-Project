254bd56e636351e619f74e2275c6e23177314d5a
Esra Güzel <esraravensburg@gmail.com>
2025-04-22 14:18:58 +0300

    create empty blade for home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('main')
+
+@php
+    $page = 'Dashboard';
+@endphp
+
+@section('content')
+
+    <div class="alert alert-soft-success d-flex align-items-center card-margin" role="alert">
+        <i class="material-icons mr-3">check</i>
+        <div class="text-body">
+            First <strong>Home Page</strong> template
+        </div>
+    </div>
+    
+@endsection
