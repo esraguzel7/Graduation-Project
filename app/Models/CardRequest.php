84ef67813562c4bdb1fa141e0ba0bb91ef1ee8a2
Esra Güzel <esraravensburg@gmail.com>
2025-04-22 16:57:36 +0300

    create card request model and table

---
+++ app/Models/CardRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CardRequest extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'reason',
+        'card_type',
+        'wallet_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function wallet()
+    {
+        return $this->belongsTo(Wallet::class);
+    }
+}
