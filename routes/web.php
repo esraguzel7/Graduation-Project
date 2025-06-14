114e303e82b458bbc602d0dd3403afc4de000f82
mhmttemin <mhmttemin@hotmail.com>
2025-06-14 16:10:40 +0300

    create artisan methods for tools usable on domain

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Auth;
 
 Route::group(['namespace' => 'App\Http\Controllers'], function () {
 
@@ -49,6 +50,26 @@ Route::group(['namespace' => 'App\Http\Controllers'], function () {
             Route::get('/logout', 'LogoutController@perform')->name('logout');
         });
 
+        Route::get('/run/{method}', function ($method) {
+            if (Auth::user()->role !== 1) {
+                abort(500);
+            }
+
+            switch ($method) {
+                case 'all':
+                    Artisan::call('migrate', ['--force' => true]);
+                    Artisan::call('db:seed', ['--force' => true]);
+                    return response()->json(['status' => 'success', 'message' => 'Migrations and seeders executed.']);
+                case 'migrate':
+                    Artisan::call('migrate', ['--force' => true]);
+                    return response()->json(['status' => 'success', 'message' => 'Migrations executed.']);
+                case 'seed':
+                    Artisan::call('db:seed', ['--force' => true]);
+                    return response()->json(['status' => 'success', 'message' => 'Seeders executed.']);
+                default:
+                    abort(404);
+            }
+        })->name('run.method');
 
         Route::group(['middleware' => ['verified']], function () {
             // Onaylı hesaplar
